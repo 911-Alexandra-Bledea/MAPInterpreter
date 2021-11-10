0234cbc1a412ffd414b91786d302f6146cbd28b7
911_Alexandra_Bledea <72063091+911-Alexandra-Bledea@users.noreply.github.com>
2021-11-10 13:58:16 +0200

    Delete A3 directory

--- A3/src/model/statement/StatementInterface.java
+++
@@ -1,9 +0,0 @@
-package model.statement;
-
-
-import model.ProgramState;
-
-public interface StatementInterface {
-    ProgramState execute(ProgramState state) throws Exception;
-    String toString();
-}
